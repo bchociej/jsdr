93e646a293da5b920e9d790e2a9890e219a23f9d
Ben Chociej <ben@chociej.io>
2016-09-30 09:36:36 -0500

    wip

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const RTLSDR = require('js-rtlsdr').RTLSDR;
 const _ = require('highland');
 
-// fastest methods I've found
+/* fastest methods I've found
 
-// suck in samples
-_('data', RTLSDR.whatever().read());
+1. suck in samples
 
-// 'type' the data
-Uint8Array.from(buffer);
-
-// NB DO NOT try to slice up stream into individual bytes
+	_('data', RTLSDR.whatever().read());
 
 
+2. 'type' the data
 
-/*
+	Uint8Array.from(buffer);
+
+
+
+
+NB DO NOT try to slice up stream into individual bytes
+
+
+
 TODO items - find highest performance solutions
 
 1. arbitrary re-sizing of TypedArrays and/or Buffers
 2. trig operations on TypedArrays
 3. mixing (multiplying) one stream with another, with incongruent chunk sizes
 4. IIR filtering at speed
+*/
